6afc2f9c44f047ef262ff6662fe7a1581c0e9d56
Luís Fernando Grange Ferreira <82682019+luisgrange@users.noreply.github.com>
2021-10-06 11:00:33 -0300

    Create script.js

---
+++ ping-coming/script.js
@@ -0,0 +1,14 @@
+function emailIsValid (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+      
+function verify(){
+  const response = emailIsValid(document.querySelector("#send").value);
+  if(!response){
+    document.querySelector("#send").style.border = "1px solid var(--Light-Red)";
+    document.querySelector("#response").innerHTML = "Please provide a valid email addres";
+  } 
+  else{
+    console.log("ok...");
+  }
+}
