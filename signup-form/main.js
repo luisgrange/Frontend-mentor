9f8589ef63c6512934cadf7ff60bc2daaadcd8ee
Luís Fernando Grange Ferreira <luisfernando237grange@outlook.com>
2021-10-29 11:53:29 -0300

    Update files

--- signup-form/main.js
+++ signup-form/main.js
@@ -12,22 +12,22 @@ $("button").on("click", function(){
 
     //validate email
     if(!emailValidation){
-        $("input#email").css("border", "1px solid rgb(249 0 0)");
+        $("input#email").css("border", "1px solid rgb(249 0 0)").addClass("onError");
         $(".errorMessage.email").css({"display": "block", "text-align": "left"});
         
     }
     else{
-        $("input#email").css({"border": "1px solid hsl(249, 10%, 26%)"});
+        $("input#email").css({"border": "1px solid hsl(249, 10%, 26%)"}).removeClass("onError");
         $(".errorMessage.email").css("display", "none");
     }
 
     //validate passwd
     if(!passwdValidation){
-        $("input#passwd").css("border", "1px solid rgb(249 0 0)");
+        $("input#passwd").css("border", "1px solid rgb(249 0 0)").addClass("onError");
         $(".errorMessage.pass").css({"display": "block", "text-align": "left"});
     }
     else{
-        $("input#passwd").css({"border": "1px solid hsl(249, 10%, 26%)"});
+        $("input#passwd").css({"border": "1px solid hsl(249, 10%, 26%)"}).removeClass("onError");
         $(".errorMessage.pass").css("display", "none");
     }
 });
